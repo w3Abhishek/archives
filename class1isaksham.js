83535f62b377a6d17d72edf2c4f5abe8fdcd405c
Abhishek Verma <abhishekverma3124@gmail.com>
2020-10-03 14:01:13 +0530

    Update class1isaksham.js

--- class1isaksham.js
+++ class1isaksham.js
@@ -1,44 +1,199 @@
-var math1= `<iframe src="https://drive.google.com/file/d/1ZkaO-hPS6YGwoiznV6EQC4fnq4BbkOK3/preview" width="640" height="480"></iframe>`;
-var math2= '<iframe src="https://drive.google.com/file/d/1_86cc86-v2eYz6B-O1MOTkh_f-Uhbf1g/preview" width="640" height="480"></iframe>';
-var math3= '<iframe src="https://drive.google.com/file/d/1F8LQXPMgIiL8kgrANagH-vKITVkVQViw/preview" width="640" height="480"></iframe>';
-var math4= '<iframe src="https://drive.google.com/file/d/1EkrEHVhLrMXY6ZDWjN9jdXUiZVc2tSTy/preview" width="640" height="480"></iframe>';
-var math5= '<iframe src="https://drive.google.com/file/d/1XSMFc7T8YTkjkgzE7X0HZTdf3HIT8nTZ/preview" width="640" height="480"></iframe>';
-var math6= '<iframe src="https://drive.google.com/file/d/1m-5fzjxYGmjqsf5GPd47IglhherwodI8/preview" width="640" height="480"></iframe>';
-var math7= '<iframe src="https://drive.google.com/file/d/1EoDYb1Yf5g_vZqFEcVz-P2epLzCmxwkn/preview" width="640" height="480"></iframe>';
-var math8= '<iframe src="https://drive.google.com/file/d/1HxpNRanJHFCU-RL_aGcwnau0PHiJIpsK/preview" width="640" height="480"></iframe>';
-var math9= '<iframe src="https://drive.google.com/file/d/14IKxAl722YwlI6yChmORYL8_nSZJPsf3/preview" width="640" height="480"></iframe>';
-var math10= '<iframe src="https://drive.google.com/file/d/1u_XIEstVT5oyCVhSswtrl1ljL6ebTGWY/preview" width="640" height="480"></iframe>';
-var math11= '<iframe src="https://drive.google.com/file/d/1gKW73iHFohCsc_IQ8aWH2n160N4kw_um/preview" width="640" height="480"></iframe>';
-var math12= '<iframe src="https://drive.google.com/file/d/16qFDOsBdTc4ua1SrEbnY-XSwKHLw8tji/preview" width="640" height="480"></iframe>';
-var math13= '<iframe src="https://drive.google.com/file/d/1uYIbjoM093JeSPdK3vfWeewKYrGNBcZZ/preview" width="640" height="480"></iframe>';
-var math14= '<iframe src="https://drive.google.com/file/d/1OJf7PGgutB1GoC3jvi8q3gdLa7WpleM8/preview" width="640" height="480"></iframe>';
-var math15= '<iframe src="https://drive.google.com/file/d/1ikLE_1qPsPq1eISAqTsa_sWiAYztTIuq/preview" width="640" height="480"></iframe>';
-var math16= '<iframe src="https://drive.google.com/file/d/1Tv8p8j1nZEBMm280el1hjd-Nn8V_Ulhs/preview" width="640" height="480"></iframe>';
-var math17= '<iframe src="https://drive.google.com/file/d/1mStGEEmSkwKtmcNDoVWDTF6UG_aLmKbm/preview" width="640" height="480"></iframe>';
-var math18= '<iframe src="https://drive.google.com/file/d/1Bwsu_J25yKN6dQV8hAbrs-5aJksJR8bN/preview" width="640" height="480"></iframe>';
-var math19= '<iframe src="https://drive.google.com/file/d/1YC87ZvueF3Uj4AlEbx9ojRrfLyDD1Ix1/preview" width="640" height="480"></iframe>';
-var math20= '<iframe src="https://drive.google.com/file/d/1HY0I5Dsb4jylIcueWzWlPIhrOLZ8wgdT/preview" width="640" height="480"></iframe>';
-var math21= '<iframe src="https://drive.google.com/file/d/1fRkBOsK0CZNU65TqlqYbvPkzN75oBsuJ/preview" width="640" height="480"></iframe>';
-var math22= '<iframe src="https://drive.google.com/file/d/1P2Obuyyu2M9TvdkGY3UDPyE-5uuv6kBZ/preview" width="640" height="480"></iframe>';
-var eng1= `<iframe src="https://drive.google.com/file/d/1ZkaO-hPS6YGwoiznV6EQC4fnq4BbkOK3/preview" width="640" height="480"></iframe>`;
-var eng2= '<iframe src="https://drive.google.com/file/d/1_86cc86-v2eYz6B-O1MOTkh_f-Uhbf1g/preview" width="640" height="480"></iframe>';
-var eng3= '<iframe src="https://drive.google.com/file/d/1F8LQXPMgIiL8kgrANagH-vKITVkVQViw/preview" width="640" height="480"></iframe>';
-var eng4= '<iframe src="https://drive.google.com/file/d/1EkrEHVhLrMXY6ZDWjN9jdXUiZVc2tSTy/preview" width="640" height="480"></iframe>';
-var eng5= '<iframe src="https://drive.google.com/file/d/1XSMFc7T8YTkjkgzE7X0HZTdf3HIT8nTZ/preview" width="640" height="480"></iframe>';
-var eng6= '<iframe src="https://drive.google.com/file/d/1m-5fzjxYGmjqsf5GPd47IglhherwodI8/preview" width="640" height="480"></iframe>';
-var eng7= '<iframe src="https://drive.google.com/file/d/1EoDYb1Yf5g_vZqFEcVz-P2epLzCmxwkn/preview" width="640" height="480"></iframe>';
-var eng8= '<iframe src="https://drive.google.com/file/d/1HxpNRanJHFCU-RL_aGcwnau0PHiJIpsK/preview" width="640" height="480"></iframe>';
-var eng9= '<iframe src="https://drive.google.com/file/d/14IKxAl722YwlI6yChmORYL8_nSZJPsf3/preview" width="640" height="480"></iframe>';
-var eng10= '<iframe src="https://drive.google.com/file/d/1u_XIEstVT5oyCVhSswtrl1ljL6ebTGWY/preview" width="640" height="480"></iframe>';
-var eng11= '<iframe src="https://drive.google.com/file/d/1gKW73iHFohCsc_IQ8aWH2n160N4kw_um/preview" width="640" height="480"></iframe>';
-var eng12= '<iframe src="https://drive.google.com/file/d/16qFDOsBdTc4ua1SrEbnY-XSwKHLw8tji/preview" width="640" height="480"></iframe>';
-var eng13= '<iframe src="https://drive.google.com/file/d/1uYIbjoM093JeSPdK3vfWeewKYrGNBcZZ/preview" width="640" height="480"></iframe>';
-var eng14= '<iframe src="https://drive.google.com/file/d/1OJf7PGgutB1GoC3jvi8q3gdLa7WpleM8/preview" width="640" height="480"></iframe>';
-var math15= '<iframe src="https://drive.google.com/file/d/1ikLE_1qPsPq1eISAqTsa_sWiAYztTIuq/preview" width="640" height="480"></iframe>';
-var eng16= '<iframe src="https://drive.google.com/file/d/1Tv8p8j1nZEBMm280el1hjd-Nn8V_Ulhs/preview" width="640" height="480"></iframe>';
-var eng17= '<iframe src="https://drive.google.com/file/d/1mStGEEmSkwKtmcNDoVWDTF6UG_aLmKbm/preview" width="640" height="480"></iframe>';
-var eng18= '<iframe src="https://drive.google.com/file/d/1Bwsu_J25yKN6dQV8hAbrs-5aJksJR8bN/preview" width="640" height="480"></iframe>';
-var eng19= '<iframe src="https://drive.google.com/file/d/1YC87ZvueF3Uj4AlEbx9ojRrfLyDD1Ix1/preview" width="640" height="480"></iframe>';
-var eng20= '<iframe src="https://drive.google.com/file/d/1HY0I5Dsb4jylIcueWzWlPIhrOLZ8wgdT/preview" width="640" height="480"></iframe>';
-var eng21= '<iframe src="https://drive.google.com/file/d/1fRkBOsK0CZNU65TqlqYbvPkzN75oBsuJ/preview" width="640" height="480"></iframe>';
-var eng22= '<iframe src="https://drive.google.com/file/d/1P2Obuyyu2M9TvdkGY3UDPyE-5uuv6kBZ/preview" width="640" height="480"></iframe>';
+var math1= `<iframe src="https://drive.google.com/file/d/1YfGHi3-O4vuIciCuMgzB1YKrSa4bYUvj/preview" width="640" height="480"></iframe>`;
+var math2= '<iframe src="https://drive.google.com/file/d/1cv0_DvHKYHc1IRQ3XE6knbXUIN7BrCJL/preview" width="640" height="480"></iframe>';
+var math3= '<iframe src="https://drive.google.com/file/d/1zmr2SNQWEjvO01RuT-rz-UhqTreFYbvy/preview" width="640" height="480"></iframe>';
+var math4= '<iframe src="https://drive.google.com/file/d/1dkPKcXmTaqR823Wd-YZJGi6tnVbjo05a/preview" width="640" height="480"></iframe>';
+var math5= '<iframe src="https://drive.google.com/file/d/1BlwTzUWOm1y2v8MKxAIKIZPmDynJLXir/preview" width="640" height="480"></iframe>';
+var math6= '<iframe src="https://drive.google.com/file/d/1f7pwVeC7E7R9NFrHIPXEkgngErRQX8mC/preview" width="640" height="480"></iframe>';
+var math7= '<iframe src="https://drive.google.com/file/d/1Jkz0-J6T7cJoA46yEv7jJ7_scg9uFY9S/preview" width="640" height="480"></iframe>';
+
+var eng1= `<iframe src="https://drive.google.com/file/d/1XF-76FS3RKl1GihvVU2J7145oxYi0TZO/preview" width="640" height="480"></iframe>`;
+var eng2= '<iframe src="https://drive.google.com/file/d/1ETsfSHVDOBccqIkGE0hYmH9OClR-YCmw/preview" width="640" height="480"></iframe>';
+var eng3= '<iframe src="https://drive.google.com/file/d/1LLdTN0zj8JgGzUiTBFWh83LQpuC3YL-i/preview" width="640" height="480"></iframe>';
+var eng4= '<iframe src="https://drive.google.com/file/d/1qgYfIvW8vuEHaLqEiWDOZf3bthMuFicQ/preview" width="640" height="480"></iframe>';
+var eng5= `<iframe src="https://drive.google.com/file/d/1VU5EwSURH-0GN2ImsQ9abyASM_lpYUA0/preview" width="640" height="480"></iframe>`;
+var eng6= '<iframe src="https://drive.google.com/file/d/1okCu63-XL6I363b6pkv0tQ17VU4RAo1p/preview" width="640" height="480"></iframe>';
+var eng7= '<iframe src="https://drive.google.com/file/d/188X6bXYgHAjL-P6LAsujQTbuG4noG0sv/preview" width="640" height="480"></iframe>';
+var eng8= '<iframe src="https://drive.google.com/file/d/1T9Z-ZboQVIfMMkQQhzloUn_i7d5WhgNe/preview" width="640" height="480"></iframe>';
+var eng9= `<iframe src="https://drive.google.com/file/d/1B9T9tUkTgGdwoyReFt00krkFXPPBo8tJ/preview" width="640" height="480"></iframe>`;
+
+var hindi1= `<iframe src="https://drive.google.com/file/d/1i6Q1azVQ54SZA_e-cNNBz1WRqbeDoqPJ/preview" width="640" height="480"></iframe>`;
+var hindi2= '<iframe src="https://drive.google.com/file/d/1LP9pWgH6dsgsDdG5gT6TEh3Cg7uGZ2Gk/preview" width="640" height="480"></iframe>';
+var hindi3= '<iframe src="https://drive.google.com/file/d/1O18zV2t47LgIWLBjSytY7Z-K0PXQdmec/preview" width="640" height="480"></iframe>';
+var hindi4= '<iframe src="https://drive.google.com/file/d/1oFi8zGbVSDgnj1c9YriVJ1Z0d5zFdNNT/preview" width="640" height="480"></iframe>';
+var hindi5= `<iframe src="https://drive.google.com/file/d/1aM1AgHBLjiASIbkVpMlbuvvsPXJ1rNnR/preview" width="640" height="480"></iframe>`;
+var hindi6= '<iframe src="https://drive.google.com/file/d/14q6S0FZmuonQodjaAZxTe_tOOFprnKgb/preview" width="640" height="480"></iframe>';
+var hindi7= '<iframe src="https://drive.google.com/file/d/10j89BmhEG0Rz-PbmM4lQYzASYH3erP8v/preview" width="640" height="480"></iframe>';
+var hindi8= '<iframe src="https://drive.google.com/file/d/1fbe0cQjPxkJks23AMdw2_xt_XD07ac-f/preview" width="640" height="480"></iframe>';
+var hindi9= `<iframe src="https://drive.google.com/file/d/1MvpY4aG6XDfoSyHZQAivlzp5ssXNjfty/preview" width="640" height="480"></iframe>`;
+
+var evs1= `<iframe src="https://drive.google.com/file/d/1i6Q1azVQ54SZA_e-cNNBz1WRqbeDoqPJ/preview" width="640" height="480"></iframe>`;
+var evs2= '<iframe src="https://drive.google.com/file/d/1jbLs4K07XRMQnfm7hwJd7t3AbXuYxvUP/preview" width="640" height="480"></iframe>';
+var evs3= '<iframe src="https://drive.google.com/file/d/1pOArH9v6lxAP6WQ5aKlluiu_pMDYuLg4/preview" width="640" height="480"></iframe>';
+var evs4= '<iframe src="https://drive.google.com/file/d/1pIi7rVt2-ymjWOq-EHm-S8JtuSnwNxKe/preview" width="640" height="480"></iframe>';
+var evs5= `<iframe src="https://drive.google.com/file/d/1iO5n6LOOPRwdbaKabgZfTxdWpVGKsL-y/preview" width="640" height="480"></iframe>`;
+var evs6= '<iframe src="https://drive.google.com/file/d/1KRr2htr0oIhPP9kjH-6pNJAMI2DXB5FK/preview" width="640" height="480"></iframe>';
+var evs7= '<iframe src="https://drive.google.com/file/d/1rtFKet1gY4Y-pg-FWKV9KMKznChYHDSA/preview" width="640" height="480"></iframe>';
+
+var sans1= `<iframe src="https://drive.google.com/file/d/1YOF36aVXK95erKMvYqwo6NktF_W1Y-w4/preview" width="640" height="480"></iframe>`;
+var sans2= '<iframe src="https://drive.google.com/file/d/1F2FXyiY-1hjtML7wrABYYqTxfR530M9S/preview" width="640" height="480"></iframe>';
+var sans3= '<iframe src="https://drive.google.com/file/d/1WUT7DFTVfWe8XaSBJKvSIIHaf0fi7yrP/preview" width="640" height="480"></iframe>';
+var sans4= '<iframe src="https://drive.google.com/file/d/1MMVhvfvGaJoK_ulQJQVnESJlx2_h3tjb/preview" width="640" height="480"></iframe>';
+var sans5= `<iframe src="https://drive.google.com/file/d/1XLcR0qgzU_Nosz2ex21ieDrZaIX6yzwk/preview" width="640" height="480"></iframe>`;
+var sans6= '<iframe src="https://drive.google.com/file/d/16VGyEPa25NiOOSmZhwCsBQGn7RF4nhxf/preview" width="640" height="480"></iframe>';
+var sans7= '<iframe src="https://drive.google.com/file/d/1SMbqS7X4K6p7uRZAvCYjajc4TcfQtT0B/preview" width="640" height="480"></iframe>';
+var sans8= '<iframe src="https://drive.google.com/file/d/1USRsCPMuJ6DhqA4DZCFNpxle6ZLPlxMc/preview" width="640" height="480"></iframe>';
+var sans9= `<iframe src="https://drive.google.com/file/d/1A78IwdZyAtiqesabWG9Fot5D0KqbuTLX/preview" width="640" height="480"></iframe>`;
+var sans10= '<iframe src="https://drive.google.com/file/d/1kAYgEtZ0RsEbJJlOJyFI0VFOoGf0l0bH/preview" width="640" height="480"></iframe>';
+var sans11= '<iframe src="https://drive.google.com/file/d/14No3XFNUIbmbfGyLMtqIS9BKf_NY7txe/preview" width="640" height="480"></iframe>';
+var sans12= '<iframe src="https://drive.google.com/file/d/1SEMFgQ0VgjfwtlyJxNZxWw97SLhRlOUz/preview" width="640" height="480"></iframe>';
+var sans13= `<iframe src="https://drive.google.com/file/d/1Drgl6Vg0E2Lh30aLC0m5rTDKog8nryt9/preview" width="640" height="480"></iframe>`;
+var sans14= '<iframe src="https://drive.google.com/file/d/1yA0v1mAemK-52y-Z_ennmMtPAgiZZmVe/preview" width="640" height="480"></iframe>';
+var sans15= '<iframe src="https://drive.google.com/file/d/1jLl_0Umk2cH0CjdVaUd6auC5K9yM1J-I/preview" width="640" height="480"></iframe>';
+var sans16= '<iframe src="https://drive.google.com/file/d/1fIqrcpdz4cIHWxSHi0wPjpcKBdfu_WdO/preview" width="640" height="480"></iframe>';
+
+var mor1 = '<iframe src="https://drive.google.com/file/d/1wNAufqTz6BbnA0dogYCAvRAKf0Z69bt4/preview" width="640" height="480"></iframe>';
+
+
+
+function LoadPDF( pdfUrl, modalId){
+const pdfHtml = `<iframe src="${pdfUrl}" width="640" height="480"></iframe>`
+
+document.getElementById(modalId).innerHTML=pdfHtml;
+
+}
+
+
+document.getElementById("class-9-english-1").addEventListener("click", ()=>{
+LoadPDF("https://drive.google.com/file/d/1ryztOGFFdz_qb6tTqgjcRAj_ful_HR1s/preview","c9e1",d)
+})
+
+/*-------------------------*/
+
+
+document.getElementById("class-9-english-2").addEventListener("click", ()=>{
+LoadPDF("https://drive.google.com/file/d/1sXjl6ot7Yf2vs1D1wcAtgidHBX9-Y04R/preview","c9e2",d)
+})
+
+/*-------------------------*/
+
+
+document.getElementById("class-9-maths").addEventListener("click", ()=>{
+LoadPDF("https://drive.google.com/file/d/19pllbUBdXsm346mRkbqDh574KxQWPKYt/preview","c9m",d)
+})
+
+/*-------------------------*/
+
+
+document.getElementById("class-9-science").addEventListener("click", ()=>{
+LoadPDF("https://drive.google.com/file/d/1555pcBXWYtsCAxz5rKJ5xRfM1fuzdj5w/preview","c9s",d)
+})
+
+/*-------------------------*/
+
+document.getElementById("class-9-hindi-1").addEventListener("click", ()=>{
+LoadPDF("https://drive.google.com/file/d/1VbwVzj1WuNQaGUnWo73ThFt8MuUm7OEq/preview","c9h1",d)
+})
+
+/*-------------------------*/
+
+document.getElementById("class-9-hindi-2").addEventListener("click", ()=>{
+LoadPDF("https://drive.google.com/file/d/14TA8oU9zYobC8b9YdhqKmmKGMNLvjuFR/preview","c9h2",d)
+})
+
+/*-------------------------*/
+
+
+document.getElementById("class-9-hindi-3").addEventListener("click", ()=>{
+LoadPDF("https://drive.google.com/file/d/1Xkt3YYVchn7lagrTYXH_CsXF6gAwPfNi/preview","c9h3",d)
+})
+
+
+
+document.getElementById("class-11-english-1").addEventListener("click", ()=>{
+LoadPDF("https://drive.google.com/file/d/1Caz6XcyupH1qhrdV3ngAkHKZ2nUdK8IG/preview","c11e1",d)
+})
+
+/*-------------------------*/
+
+document.getElementById("class-11-english-2").addEventListener("click", ()=>{
+LoadPDF("https://drive.google.com/file/d/1RDpsKCP86gEnZOzpneB8XO5PT4h8aQQG/preview","c11e2",d)
+})
+
+/*-------------------------*/
+document.getElementById("class-11-chemistry-1").addEventListener("click", ()=>{
+LoadPDF("https://drive.google.com/file/d/1NGtFyhruEc20gIBX8-1astb79I4vmGF4/preview","c11c1",d)
+})
+
+/*-------------------------*/
+
+document.getElementById("class-11-chemistry-2").addEventListener("click", ()=>{
+LoadPDF("https://drive.google.com/file/d/11j-DLQ_Zn8VJWkBIK5xKns8eP3xs2Bf9/preview","c11c2",d)
+})
+
+/*-------------------------*/
+
+document.getElementById("class-11m").addEventListener("click", ()=>{
+LoadPDF("https://drive.google.com/file/d/1Ijyt_x6-fcWrOrS8C7wK9OCjtgj78Q2j/preview","c11m",d)
+})
+
+/*-------------------------*/
+
+document.getElementById("class-11-p1").addEventListener("click", ()=>{
+LoadPDF("https://drive.google.com/file/d/1uBcFD7AwrJJ0GaIeJowRkXovN-ufBJ1S/preview","c11p1")
+})
+
+/*-------------------------*/
+
+document.getElementById("class-11-p2").addEventListener("click", ()=>{
+LoadPDF("https://drive.google.com/file/d/1Kk7TSHTpv47T0V6JDQxN2hhAU-KgCGub/preview","c11p2")
+})
+
+/*-------------------------*/
+
+document.getElementById("class-12-e1").addEventListener("click", ()=>{
+LoadPDF("https://drive.google.com/file/d/1tR1YayakWV-B2j-CN_qua1Pcv2q585Hw/preview","c12e1")
+})
+
+/*-------------------------*/
+
+document.getElementById("class-12-e2").addEventListener("click", ()=>{
+LoadPDF("https://drive.google.com/file/d/1YnJfmF1ga3gYWhySw595H-6LihxCnC2t/preview","c12e2")
+})
+
+/*-------------------------*/
+
+document.getElementById("class-12-p1").addEventListener("click", ()=>{
+LoadPDF("https://drive.google.com/file/d/1Rw1wdl6G_a3LH-98U1MYBmTI6f9kJWJm/preview","c12p1")
+})
+
+/*-------------------------*/
+
+document.getElementById("class-12-p2").addEventListener("click", ()=>{
+LoadPDF("https://drive.google.com/file/d/1IqcLHjtMvJe2axtqcHBNlbPxigE-idIB/preview","c12p2")
+})
+
+/*-------------------------*/
+
+document.getElementById("c12m11").addEventListener("click", ()=>{
+LoadPDF("https://drive.google.com/file/d/1BM0v12m4WMXGU2MkAmcXkeOIH9vLf6X6/preview","c12m1")
+})
+
+/*-------------------------*/
+
+
+document.getElementById("class-12-m2").addEventListener("click", ()=>{
+LoadPDF("https://drive.google.com/file/d/1Nhq1ZtQD5thzI39A5chiLKC1OFAEQ6SP/preview","c12m2")
+})
+
+/*-------------------------*/
+
+document.getElementById("class-12-c1").addEventListener("click", ()=>{
+LoadPDF("https://drive.google.com/file/d/17uDTCyx0YE270TJr91yN5YDDkgrSYgkW/preview","c12c1")
+})
+
+/*-------------------------*/
+
+
+document.getElementById("class-12-c2").addEventListener("click", ()=>{
+LoadPDF("https://drive.google.com/file/d/1Yj1cWX-DjtC3Q-ertQPf_P09QO3yHW4e/preview","c12c2")
+})
